178fec27727d8bfa4da31eaaa12e1c9e74ede284
Gummigrodan <isacericlarsson@outlook.com>
2025-11-09 12:42:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -49,6 +49,8 @@ app.post("/booths/:id/donate", (req, res) => {
     res.json(booth);
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
